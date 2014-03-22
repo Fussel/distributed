783db4ec5055424f55cc272914bf730b087374c7
Simon Gloxhuber <dark0ni1990@gmail.com>
2014-03-23 00:15:11 +0100

    + IP List maker

---
+++ Distributed/src/distributed/util/IpAdressAdapter.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributed.util;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+/**
+ *
+ * @author b0n3h_000
+ */
+public class IpAdressAdapter {
+    
+    /*
+    * Create a ip list with the active Ip
+    */
+    public ArrayList<String> getNetworkInterfaces() throws SocketException {
+        System.out.println("MEINE TESTAREA");
+        ArrayList<String> ipList = new ArrayList<String>();
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface current = interfaces.nextElement();
+            //System.out.println(current);
+            if (!current.isUp() || current.isLoopback() || current.isVirtual()) {
+                continue;
+            }
+            Enumeration<InetAddress> addresses = current.getInetAddresses();
+
+            while (addresses.hasMoreElements()) {
+                InetAddress current_addr = addresses.nextElement();
+                if (current_addr.isLoopbackAddress()) {
+                    continue;
+                }
+
+                if (current_addr instanceof Inet4Address) {
+                    System.out.println(current_addr.getHostAddress());
+                    ipList.add(current_addr.getHostAddress());
+                } /*else if (current_addr instanceof Inet6Address) {
+                 System.out.println(current_addr.getHostAddress());
+                 }*/
+
+            }
+        }
+        System.out.println("test end");
+        return ipList;
+    }
+    
+    /*
+    *  Return a array with seperated numbers of the ip
+    */
+    public String[] getSeperatedNumber(int position, ArrayList<String> ipList) {
+        String ip = ipList.get(position);
+        String[] seperated = ip.split(".");
+        return seperated;
+    }
+}
