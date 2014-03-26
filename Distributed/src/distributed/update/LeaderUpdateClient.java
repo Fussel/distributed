dbc5ad75bbf5a9b554096b20bc3e036c55893ec9
steffen <steffen@Latitude>
2014-03-26 13:48:03 +0100

    Update classes added

---
+++ Distributed/src/distributed/update/LeaderUpdateClient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package distributed.update;
+
+/**
+ *
+ * @author steffen
+ */
+public class LeaderUpdateClient extends IncrementalUpdate.UpdateClient {
+
+    public LeaderUpdateClient(String ip, int port) {
+        super(ip, port);
+    }
+    
+}
---
+++ Distributed/src/distributed/update/UpdateServer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package distributed.update;
+
+/**
+ *
+ * @author steffen
+ */
+public class UpdateServer extends IncrementalUpdate.UpdateServer {
+
+    public UpdateServer(int port) {
+        super(port);
+    }
+    
+}
